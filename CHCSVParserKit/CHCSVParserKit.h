2d987c82fe56dfdf3ca2ee78d52e765169cd2c71
Juergen Falb <j.falb@nousdigital.net>
2020-02-29 14:24:55 +0100

    Added shared framework

---
+++ CHCSVParserKit/CHCSVParserKit.h
@@ -0,0 +1,17 @@
+//
+//  CHCSVParserKit.h
+//  CHCSVParserKit
+//
+//  Created by Juergen Falb on 29.02.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CHCSVParserKit.
+FOUNDATION_EXPORT double CHCSVParserKitVersionNumber;
+
+//! Project version string for CHCSVParserKit.
+FOUNDATION_EXPORT const unsigned char CHCSVParserKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHCSVParserKit/PublicHeader.h>
+#import <CHCSVParserKit/CHCSVParser.h>
